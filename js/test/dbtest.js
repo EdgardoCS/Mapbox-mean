f7462225063c3c193ba8d8015c9cabeeca9f48c5
EdgardoCS <edgardo_ches@hotmail.com>
2018-06-08 16:44:33 -0400

    ignore

--- js/test/dbtest.js
+++ js/test/dbtest.js
@@ -7,100 +7,101 @@ MongoClient.connect(url, {
   if (err) throw err;
   var ndb = db.db("mydb");
 
-  // var obj = [{
-  //     name: "BadBadNotGood",
-  //     album: "III"
-  //   },
-  //   {
-  //     name: "Robohands",
-  //     album: "Green"
-  //   },
-  //   {
-  //     name: "Santana",
-  //     album: "Santana"
-  //   }, {
-  //     name: "Psychedelic Porn Crumpets",
-  //     album: "High Visceral"
-  //   },
-  //   {
-  //     name: "Santana",
-  //     album: "Abraxas"
-  //   }
-  // ];
+  var obj = [{
+      name: "BadBadNotGood",
+      album: "III"
+    },
+    {
+      name: "Robohands",
+      album: "Green"
+    },
+    {
+      name: "Santana",
+      album: "Santana"
+    }, {
+      name: "Psychedelic Porn Crumpets",
+      album: "High Visceral"
+    },
+    {
+      name: "Santana",
+      album: "Abraxas"
+    }
+  ];
 
   /* INSERT */
   // ndb.collection("music").insertMany(obj, function(err, res) {
-  //   if (err) throw err;
-  //   console.log(res.insertedCount + " objetos insertados a coleccion music");
+    // if (err) throw err;
+    // console.log(res.insertedCount + " objetos insertados a coleccion music");
 
-  /* FIND */
-  // ndb.collection("music").find({}).toArray(function(err, result) {
-  //   if (err) throw err;
-  //   if (result.length == 0) {
-  //     console.log("coleccion no encontrada");
-  //   } else {
-  //     console.log(result);
-  //   }
+    /* FIND */
+    ndb.collection("music").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      if (result.length == 0) {
+        console.log("coleccion no encontrada");
+      } else {
+        // console.log(result);
+    //   }
 
-  /* QUERY */
-  // var query = {
-  //   album: "III"
-  // };
-  // ndb.collection("music").find(query).toArray(function(err, result) {
-  //   if (err) throw err;
+    /* QUERY */
+    // var query = {
+    //   album: "III"
+    // };
+    // ndb.collection("music").find(query).toArray(function(err, result) {
+    //   if (err) throw err;
 
-  // var _l = result.length;
-  // for (i = 0; i < _l; i++) {
-  // console.log(result[i]);
-  // }
+      var _l = result.length;
+      for (i = 0; i < _l; i++) {
+      console.log(result[i]);
+      }
+    }
 
-  /* SORT */
-  // var orden = {
-  //   name: -1
-  // };
-  // ndb.collection("music").find().sort(orden).toArray(function(err, result) {
-  //   if (err) throw err;
-  //   console.log(result);
-  //
-  //   db.close();
-  // });
+    /* SORT */
+    // var orden = {
+    //   name: -1
+    // };
+    // ndb.collection("music").find().sort(orden).toArray(function(err, result) {
+    //   if (err) throw err;
+    //   console.log(result);
+    //
+    //   db.close();
+    // });
 
-  /* DELETE */
-  // var trash = {
-  //   name: 'badbadnotgood'
-  // };
-  // ndb.collection("music").deleteOne(trash, function(err, obj) {
-  //   if (err) throw err;
-  //n es la cantidad de objetos eliminados
-  //   console.log("objetos borrados: " + obj.result.n);
+    /* DELETE */
+    // var trash = {
+    //   name: 'badbadnotgood'
+    // };
+    // ndb.collection("music").deleteOne(trash, function(err, obj) {
+    //   if (err) throw err;
+    //n es la cantidad de objetos eliminados
+    //   console.log("objetos borrados: " + obj.result.n);
 
-  /* DELETE COLLECTION */
-  // ndb.collection("music").drop(function(err, delOK) {
-  //   if (err) throw err;
-  //   if (delOK) {
-  //     console.log("Coleccion music borrada");
-  //   }
+    /* DELETE COLLECTION */
+    // ndb.collection("music").drop(function(err, delOK) {
+    //   if (err) throw err;
+    //   if (delOK) {
+    //     console.log("Coleccion music borrada");
+    //   }
 
-  /* UPDATE */
-  // var oldDisc = {
-  //   album: "High Visceral"
-  // };
-  // var newDisc = {
-  //   $set: {
-  //     album: "High Visceral II"
-  //   }
-  // };
-  // ndb.collection("music").updateOne(oldDisc, newDisc, function(err, result) {
-  //   if (err) throw err;
-  //   console.log("coleccion actualizada");
-  // });
-  // ndb.collection("music").find({}).toArray(function(err, result) {
-  //   if (err) throw err;
-  //   if (result.length == 0) {
-  //     console.log("coleccion no encontrada");
-  //   } else {
-  //     console.log(result);
-  //   }
+    /* UPDATE */
+    // var oldDisc = {
+    //   album: "High Visceral"
+    // };
+    // var newDisc = {
+    //   $set: {
+    //     album: "High Visceral II"
+    //   }
+    // };
+    // ndb.collection("music").updateOne(oldDisc, newDisc, function(err, result) {
+    //   if (err) throw err;
+    //   console.log("coleccion actualizada");
+    // });
+    // ndb.collection("music").find({}).toArray(function(err, result) {
+    //   if (err) throw err;
+    //   if (result.length == 0) {
+    //     console.log("coleccion no encontrada");
+    //   } else {
+    //     console.log(result);
+    //   }
 
     /*############################################*/
     db.close();

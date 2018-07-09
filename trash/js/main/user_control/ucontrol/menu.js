98b69da7f9eefd9cae1bb3289dbad7fcfb026fce
EdgardoCS <edgardo_ches@hotmail.com>
2018-07-09 17:10:43 -0400

    Remove

--- trash/js/main/user_control/ucontrol/menu.js
+++
@@ -1,8 +0,0 @@
-  $(document).ready(function() {
-    $(".deploymenu").click(function() {
-      $(this).children("ul").slideToggle();
-    });
-    $("ul").click(function(p) {
-      p.stopPropagation();
-    });
-  });

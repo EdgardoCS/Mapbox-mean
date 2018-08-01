4e9b393f3021e0e8078364f99a02e33d2b6aee11
EdgardoCS <edgardo_ches@hotmail.com>
2018-08-01 17:54:42 -0400

    almost

--- js/test.js
+++ js/test.js
@@ -1,31 +1,119 @@
-var disc = [
-    ['dance of death', 0, 11],
-    ['brave new world', 12, 22]
-];
-var songs = [
-    ['rainmaker', 1],
-    ['face in the sand', 8],
-    ['the wickerman', 13],
-    ['out of the silent planet', 21]
-];
+//toparser = function (f) {
+file_path = '/home/geropolis/Documents/Projects/Mapbox-mean/src/xlsx/example/PSCV.xlsx';
 
-console.log(disc);
-console.log(songs);
+//  file_name = f.name;
+//  file_path = f.path;
 
-for (i = 0; i < disc.length; i++) {
-    for (j = 0; j < songs.length; j++) {
-        //console.log(disc[i])
-        //console.log(songs[j])
+goToPath = file_path;
+
+if (typeof require !== 'undefined') XLSX = require('xlsx');
+var workbook = XLSX.readFile(goToPath);
+
+var _hl = workbook.SheetNames.length;
+for (h = 0; h < _hl; h++) {
+    hojas = workbook.SheetNames[h];
+}
+var worksheet = workbook.Sheets[hojas];
+
+var workspace = XLSX.utils.sheet_to_json(worksheet, {
+    header: 1,
+});
+var _w = workspace.length;
+
+var mheader_inicio = [];
+var mheader_fin = [];
+
+var merge = worksheet["!merges"];
+var _m = merge.length;
+
+var currentwork = [];
+
+var group_headers = [];
+var common_headers = [];
+var shared_headers = [];
+
+invitation = function (join, shared_headers, currentwork) {
+
+    console.log(join);
+    console.log(shared_headers);
+    console.log(currentwork); 
+
+    var final = [];
+/*
+    for (c = 0; c < join.length; c++) {
+        head = 'shared_headers[c]'
+        for (d = 0; d < join[c].length; d ++){
+        Object.assign(final, {
+            [eval(head)]: {
+                [eval('join[c][d]')]: "",
+            }
+        })
     }
 }
+*/
+    console.log(final);
 
-for (h = 0; h < disc.length; h++) {
-    for (i = disc[h][1]; i <= disc[h][2]; i++) {
-        for (j = 0; j < songs.length; j++){
-            if (songs[j][1] == i) {
-                console.log(songs[j][0])
+}
+
+widows_needs = function (group_headers, shared_headers, common_headers, currentwork) {
+    var common = [];
+    var join = [];
+
+    for (a = 0; a < group_headers.length; a++) {
+        header = shared_headers[a];
+        inicio = group_headers[a][0];
+        final = group_headers[a][1] + 1;
+        medio = final - inicio;
+
+        for (b = inicio; b <= final; b++) {
+            medio = final - inicio;
+
+            if (common.length < medio) {
+                common.push(common_headers[b])
+                join[a] = common;
+            } else {
+                common = [];
             }
         }
-        console.log(i);
     }
+    invitation(join, shared_headers, currentwork);
+
+    /*
+    console.log(group_headers);
+    console.log(common_headers)
+    console.log(currentwork);
+    */
 }
+
+for (co = 0; co < _w; co++) {
+    _c = workspace[co].length;
+    if (co == 0) {
+        for (c = 0; c < _c; c++) {
+            headers = workspace[co][c]
+            if (headers != undefined) {
+                for (m = 0; m < _m; m++) {
+                    mheader_inicio[m] = merge[m].s.c; //inicio celda compartida
+                    mheader_fin[m] = merge[m].e.c; //fin celda compartida
+
+                    if (c == mheader_inicio[m]) {
+
+                        inicio = mheader_inicio[m];
+                        fin = mheader_fin[m];
+                        group_headers[m] = [inicio, fin]
+                        shared_headers[m] = headers
+                    };
+                };
+            };
+        };
+    };
+    if (co == 1) {
+        for (c = 0; c < _c; c++) {
+            common_headers[c] = workspace[co][c]
+        }
+    };
+    if (co > 1) {
+        currentwork[co] = workspace[co]
+    };
+};
+widows_needs(group_headers, shared_headers, common_headers, currentwork);
+//}
